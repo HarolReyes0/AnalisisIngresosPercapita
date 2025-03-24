4142409da869c5a172a9f63bd9a9819cb9d26296
Harol Reyes <harolreyes0@gmail.com>
2025-03-24 01:31:03 -0400

    FEAT: Fuction to generate all charts created.

--- src/dashboard/layout.py
+++ src/dashboard/layout.py
@@ -7,18 +7,61 @@ from charts import *
 
 
 def load_data():
+    """
+        Loads and processes Excel datasets from predefined file paths, and sets global variables.
+
+        This function performs the following tasks:
+        - Loads three Excel files into global DataFrames: `one0`, `one1`, and `one2`.
+        - Sets a global float display format to two decimal places for all pandas DataFrames.
+        - Extracts all unique years from the 'año' column across the three datasets and stores them 
+        in a global set variable `available_years`.
+
+        Global Variables:
+        - one0 (pd.DataFrame): Data loaded from 'one 0.xlsx'.
+        - one1 (pd.DataFrame): Data loaded from 'one 1.xlsx'.
+        - one2 (pd.DataFrame): Data loaded from 'one 2.xlsx'.
+        - available_years (set): Set of all unique years found in the 'año' column across all three datasets.
+
+        Note:
+            The function assumes the Excel files are located relative to the script path at:
+            '../../data/processed/one/' and each file contains a column named 'año'.
+    """
     global one0, one1, one2, available_years
 
+    # Reading all files
     one0 = pd.read_excel('../../data/processed/one/one 0.xlsx')
     one1 = pd.read_excel('../../data/processed/one/one 1.xlsx')
     one2 = pd.read_excel('../../data/processed/one/one 2.xlsx')
     
     pd.options.display.float_format = '{:.2f}'.format
 
+    # Obtaining the list of available years.
     available_years = {(x) for df in [one0, one1, one2] for x in df['año'].unique().tolist()}
 
 def create_charts(filter_: str, years: list) -> None:
-    pass
+    """
+        Generates a collection of charts based on provided filter and years.
+
+        This function creates multiple visualizations using pre-loaded datasets (`one0`, `one1`, `one2`)
+        and a the typer of regime and list of years. Each chart corresponds to a specific aspect of the data.
+
+        Parameters:
+        - filter_ (str): A string filter used to segment or filter the data.
+        - years (list): A list of years (int) used to filter the data before generating the charts.
+
+        Returns:
+        - tuple: A tuple containing the generated figures in the following order.
+    """
+    # Constructing all charts.
+    figs = (
+        cust_amt_per_capitation_type(one0, filter_, years),
+        amt_capitation_paid_per_cust_type(one0, filter_, years),
+        amt_capitation_per_gender(one1, filter_, years),
+        capitation_amt_per_cust_type(one2, years),
+        pct_money_per_cust_type(one2, years),
+    )
+
+    return figs
 
 def show_dashboard():
     # Creating the dashboard layout.
@@ -70,3 +113,4 @@ def show_dashboard():
 if __name__ == '__main__':
     load_data()
     show_dashboard()
+    create_charts('', [])
