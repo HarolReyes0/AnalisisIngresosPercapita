d50f7e99fd0c1b2c64157505741998ab29b95331
Harol Reyes <harolreyes0@gmail.com>
2025-03-24 22:29:27 -0400

    FEAT: Creating the final structe to run the proyect.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-import dash_bootstrap_components as dbc
+from modeling.data_cleaning import ModelData
+from dashboard.layout import load_data, show_dashboard
+
+def main():
+    # Modeling and saving the data.
+    data_modeler = ModelData()
+    data_modeler.model_and_save_data()
+
+    # Loading the data.
+    load_data()
+
+    # Displaying the dashboard.
+    show_dashboard()
+
+if __name__ == '__main__':
+    main()
